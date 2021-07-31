8c977fd2cb6bc45b2fc78f0b15956ba839a7ee1b
atomiyama <tomisuker16@gmail.com>
2021-07-31 23:14:01 +0900

    feat: Add Method for getting client from token file

--- auth/auth.go
+++ auth/auth.go
@@ -2,10 +2,14 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"os"
 
 	"golang.org/x/oauth2"
 )
+
+type GetClientOptions map[string]string
 
 func AuthorizeURL(cnf *oauth2.Config) string {
 	return cnf.AuthCodeURL("state", oauth2.AccessTypeOffline)
@@ -16,6 +20,16 @@ func GetClientFromCode(ctx context.Context, cnf *oauth2.Config, authCode string)
 	if err != nil {
 		return nil, err
 	}
+	return cnf.Client(ctx, token), nil
+}
 
+func GetClientFromFile(ctx context.Context, tokenFile string, cnf *oauth2.Config) (*http.Client, error) {
+	f, err := os.Open(tokenFile)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var token *oauth2.Token
+	json.NewDecoder(f).Decode(token)
 	return cnf.Client(ctx, token), nil
 }
